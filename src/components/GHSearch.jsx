c3d4044fd643ae966bcdbfa40a88b2c63cf4785f
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-25 10:53:21 +0100

    Stores value of input field in state

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Button, Input } from 'semantic-ui-react'
+import axios from 'axios'
 
 const GHSearch = () => {
+  const [searchValue, setSearchValue] = useState()
+
+
   return (
     <>
-      <Input data-cy="search-bar" type="text" name="search" placeholder="Input GH username"/>
+      <Input onChange={(event) => {setSearchValue(event.target.value)}} data-cy="search-bar" type="text" name="search" placeholder="Input GH username"/>
       <Button data-cy="submit" name="search">Search</Button>
     </>
   )
